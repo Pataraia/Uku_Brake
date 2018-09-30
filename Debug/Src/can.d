2cf9e70469a3c15ecef7d3f30eb87d1bc8e608c2
Pataraia <tegapataraia@gmail.com>
2018-09-30 23:32:12 +0300

    Initial commit 	#

---
+++ Debug/Src/can.d
@@ -0,0 +1,96 @@
+Src/can.o: ../Src/can.c /home/iseauto/Desktop/Brake/Inc/can.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ /home/iseauto/Desktop/Brake/Inc/stm32f3xx_hal_conf.h \
+ /home/iseauto/Desktop/Brake/Inc/main.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cm4.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h \
+ /home/iseauto/Desktop/Brake/Inc/gpio.h
+
+/home/iseauto/Desktop/Brake/Inc/can.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+/home/iseauto/Desktop/Brake/Inc/stm32f3xx_hal_conf.h:
+
+/home/iseauto/Desktop/Brake/Inc/main.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h:
+
+/home/iseauto/Desktop/Brake/Inc/gpio.h:
---
+++ Debug/Src/usbd_cdc_if.d
@@ -0,0 +1,118 @@
+Src/usbd_cdc_if.o: ../Src/usbd_cdc_if.c \
+ /home/iseauto/Desktop/Brake/Inc/usbd_cdc_if.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ /home/iseauto/Desktop/Brake/Inc/usbd_conf.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cm4.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmInstr.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmFunc.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmSimd.h \
+ /home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h \
+ /home/iseauto/Desktop/Brake/Inc/stm32f3xx_hal_conf.h \
+ /home/iseauto/Desktop/Brake/Inc/main.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h \
+ /home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h \
+ /home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h
+
+/home/iseauto/Desktop/Brake/Inc/usbd_cdc_if.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Class/CDC/Inc/usbd_cdc.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+/home/iseauto/Desktop/Brake/Inc/usbd_conf.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f3xx.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/stm32f303xc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cm4.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/home/iseauto/Desktop/Brake/Drivers/CMSIS/Device/ST/STM32F3xx/Include/system_stm32f3xx.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal.h:
+
+/home/iseauto/Desktop/Brake/Inc/stm32f3xx_hal_conf.h:
+
+/home/iseauto/Desktop/Brake/Inc/main.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_def.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_rcc_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_gpio_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_dma_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_cortex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_can.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_flash_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_i2c_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pcd_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_pwr_ex.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim.h:
+
+/home/iseauto/Desktop/Brake/Drivers/STM32F3xx_HAL_Driver/Inc/stm32f3xx_hal_tim_ex.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_def.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_core.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ioreq.h:
+
+/home/iseauto/Desktop/Brake/Middlewares/ST/STM32_USB_Device_Library/Core/Inc/usbd_ctlreq.h:
